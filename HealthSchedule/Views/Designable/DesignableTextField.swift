527d3e9b9167b64059cb03ae3ea40b70b3de650d
Andrey Popazov <andrey.plagiatix@gmail.com>
2019-01-18 17:07:40 +0200

    Flexible design for with portraint mode

--- HealthSchedule/Views/Designable/DesignableTextField.swift
+++ HealthSchedule/Views/Designable/DesignableTextField.swift
@@ -11,6 +11,13 @@ import UIKit
 @IBDesignable
 class DesignableTextField: UITextField {
   
+  // Provides left padding for images
+  override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+    var textRect = super.leftViewRect(forBounds: bounds)
+    textRect.origin.x += leftPadding
+    return textRect
+  }
+  
   @IBInspectable var leftImage: UIImage? {
     didSet {
       updateView()
@@ -25,23 +32,16 @@ class DesignableTextField: UITextField {
     }
   }
   
-  // Provides left padding for images
-  override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
-    var textRect = super.leftViewRect(forBounds: bounds)
-    textRect.origin.x += leftPadding
-    return textRect
-  }
-  
   func updateView() {
     if let image = leftImage {
-      leftViewMode = .always
+      leftViewMode = UITextField.ViewMode.always
       let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
       imageView.contentMode = .scaleAspectFit
       imageView.image = image
       imageView.tintColor = color
       leftView = imageView
     } else {
-      leftViewMode = .never
+      leftViewMode = UITextField.ViewMode.never
       leftView = nil
     }
     
