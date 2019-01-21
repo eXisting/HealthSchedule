a9555e256fddbfb70ea2c3044efe0963fbbf5df3
evgeniy <jheka524@gmail.com>
2019-01-21 10:27:39 +0200

    add migrations

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->integer('user_role_id');
+            $table->integer('profession_id');
+            $table->integer('cyity_id');
+            $table->integer('first_name');
+            $table->integer('last_name');
+            $table->string('phone');
+            $table->tinyInteger('confirmed_status');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
