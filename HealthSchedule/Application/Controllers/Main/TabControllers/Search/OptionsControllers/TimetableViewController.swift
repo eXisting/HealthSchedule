6db1a378cd6cc31e89e54a0013e6ad2426620820
Andrey Popazov <andrey.plagiatix@gmail.com>
2019-03-13 17:02:14 +0200

    Added Calendar with pods

--- HealthSchedule/Application/Controllers/Main/TabControllers/Search/OptionsControllers/TimetableViewController.swift
+++ HealthSchedule/Application/Controllers/Main/TabControllers/Search/OptionsControllers/TimetableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FSCalendar
 
 class TimetableViewController: UIViewController {
   private let titleName = "Date and Time"
@@ -14,5 +15,14 @@ class TimetableViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     navigationItem.title = titleName
+    
+    let calendar = FSCalendar(frame: CGRect(x: 30, y: 300, width: self.view.frame.width * 0.9, height: 300))
+    calendar.dataSource = self
+    calendar.delegate = self
+    view.addSubview(calendar)
   }
+}
+
+extension TimetableViewController: FSCalendarDelegate, FSCalendarDataSource {
+  
 }
---
+++ Pods/Target Support Files/Pods-HealthSchedule/Pods-HealthSchedule-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HealthSchedule : NSObject
+@end
+@implementation PodsDummy_Pods_HealthSchedule
+@end
