46d83e636cf37c5f0d8f03a7e4c730b8d67b52cd
Andrey Popazov <andrey.plagiatix@gmail.com>
2019-03-14 13:06:49 +0200

    Search rows indicators added

--- HealthSchedule/Application/UI/Views/Main/Search/TableView/SearchOptionRow.swift
+++ HealthSchedule/Application/UI/Views/Main/Search/TableView/SearchOptionRow.swift
@@ -11,6 +11,15 @@ import UIKit
 class SearchOptionRow: UITableViewCell {
   // TODO: RemoteService object
   
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    accessoryType = .disclosureIndicator
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   func setChosenValue(value: String) {
     self.textLabel?.text?.append(" - (\(value))")
   }
