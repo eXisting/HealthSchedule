2d364bbbf49d480ff512ad94cd10abb875ac5546
Andrey Popazov <andrey.plagiatix@gmail.com>
2019-03-16 08:10:18 -0700

    Safe area layout guide for ios 10+

--- HealthSchedule/Application/Tools/SystemExtensions.swift
+++ HealthSchedule/Application/Tools/SystemExtensions.swift
@@ -44,3 +44,29 @@ extension Dictionary where Key == String, Value == String {
     return "?" + parameterArray.joined(separator: "&")
   }
 }
+
+protocol LayoutGuideProvider {
+  var leadingAnchor: NSLayoutXAxisAnchor { get }
+  var trailingAnchor: NSLayoutXAxisAnchor { get }
+  var leftAnchor: NSLayoutXAxisAnchor { get }
+  var rightAnchor: NSLayoutXAxisAnchor { get }
+  var topAnchor: NSLayoutYAxisAnchor { get }
+  var bottomAnchor: NSLayoutYAxisAnchor { get }
+  var widthAnchor: NSLayoutDimension { get }
+  var heightAnchor: NSLayoutDimension { get }
+  var centerXAnchor: NSLayoutXAxisAnchor { get }
+  var centerYAnchor: NSLayoutYAxisAnchor { get }
+}
+
+extension UIView: LayoutGuideProvider { }
+extension UILayoutGuide: LayoutGuideProvider { }
+
+extension UIView {
+  var compatibleSafeAreaLayoutGuide: LayoutGuideProvider {
+    if #available(iOS 11, *) {
+      return safeAreaLayoutGuide
+    } else {
+      return self
+    }
+  }
+}
